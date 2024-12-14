9a39d89e578b0587e04288fcdade2626af466536
rajon-git <rajon.zhsust15@gmail.com>
2024-12-14 22:05:20 +0600

    project member also create

--- projects/models.py
+++ projects/models.py
@@ -10,3 +10,19 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProjectMember(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Member', 'Member'),
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="members")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="project_memberships")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    class Meta:
+        unique_together = ('project', 'user')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role} in {self.project.name}"
