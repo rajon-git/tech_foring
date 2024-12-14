16bd05e81ba1efe296a171c28a011e90110662c1
rajon-git <rajon.zhsust15@gmail.com>
2024-12-14 22:08:31 +0600

    tasks comment create

--- tasks/models.py
+++ tasks/models.py
@@ -25,3 +25,13 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="comments")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.task.title}"
