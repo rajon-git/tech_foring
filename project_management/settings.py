e9556bffe4707608cc4884ed845001aa38473b55
rajon-git <rajon.zhsust15@gmail.com>
2024-12-14 22:07:45 +0600

    tasks model create

--- project_management/settings.py
+++ project_management/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'projects',
     'userauths',
+    'tasks',
     'rest_framework',
 ]
 
---
+++ tasks/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from userauths.models import User
+from projects.models import Project
+
+# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('To Do', 'To Do'),
+        ('In Progress', 'In Progress'),
+        ('Done', 'Done'),
+    )
+    PRIORITY_CHOICES = (
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='To Do')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='Medium')
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="tasks_assigned")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
